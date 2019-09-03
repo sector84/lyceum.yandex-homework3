fba4f52fadb3c65dc170e4a8ba887be6b2f4c6d5
Kovalev Ivan <ikovalew@gmail.com>
2019-09-03 18:52:22 +0300

    wip: рисуем флагшток
    чтобы это ни значило  😫

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ screen = pygame.display.set_mode(size)
 
 
 def draw():
-    pass
+    green = pygame.Color('green')
+    points = [(10, 10), (15, 280)]
+    pygame.draw.rect(screen, green, points, 0)
 
 
 if __name__ == '__main__':
